769e47f1f291c615bd2689da096c995f7b970429
hosam mohamed <hosamelmalt@gmail.com>
2020-12-05 12:22:30 -0800

    Initial Commit

---
+++ Wheel Spinal Raidal/Wheel_Spinal_RaidalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wheel_Spinal_RaidalApp.swift
+//  Wheel Spinal Raidal
+//
+//  Created by Hossam on 12/5/20.
+//
+
+import SwiftUI
+
+@main
+struct Wheel_Spinal_RaidalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
